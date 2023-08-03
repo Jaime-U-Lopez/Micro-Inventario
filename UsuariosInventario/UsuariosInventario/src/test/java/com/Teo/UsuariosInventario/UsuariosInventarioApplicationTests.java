3e20427df384d432825ee4121c6f6b8c2dc54156
jaime urrego <or4846@gmail.com>
2023-08-03 18:55:41 -0500

    first commit

---
+++ UsuariosInventario/UsuariosInventario/src/test/java/com/Teo/UsuariosInventario/UsuariosInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Teo.UsuariosInventario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuariosInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
