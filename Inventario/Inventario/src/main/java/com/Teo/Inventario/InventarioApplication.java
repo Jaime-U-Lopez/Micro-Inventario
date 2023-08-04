47e6e6b141a7b56368b1186014dba91de9755785
jaime urrego <or4846@gmail.com>
2023-08-04 12:14:00 -0500

    Update librerias

--- Inventario/Inventario/src/main/java/com/Teo/Inventario/InventarioApplication.java
+++ Inventario/Inventario/src/main/java/com/Teo/Inventario/InventarioApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InventarioApplication {
-//todo
+
 	public static void main(String[] args) {
 		SpringApplication.run(InventarioApplication.class, args);
 	}
